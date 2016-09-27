a9bf29de80f309258fecab0f0bcb4ba1d60a2014
Arnold Vriezekolk <arnold.vriezekolk@drukwerkdeal.nl>
2016-09-27 18:30:11 +0200

    Added example shit - for testing purposes only

---
+++ example_window.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+import sys
+
+from PyQt4.QtWebKit import QWebView
+from PyQt4.QtGui import QApplication
+from PyQt4.QtCore import QUrl
+from BeautifulSoup import BeautifulSoup
+from markdown import markdown
+
+
+app = QApplication(sys.argv)
+
+md = open(sys.argv[1]).read()
+
+html = markdown(md)
+
+browser = QWebView()
+browser.setHtml(html)
+#browser.load(QUrl(sys.argv[1]))
+browser.show()
+
+app.exec_()
