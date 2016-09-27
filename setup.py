e9a0c239f43c28f295b048fb56e5edb5f9096720
Arnold Vriezekolk <arnold.vriezekolk@drukwerkdeal.nl>
2016-09-27 17:19:33 +0200

    Added setup and mdbrowser directory

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='MDbrowser',
+      version='0.1',
+      description='MarkDown Browser',
+      url='http://github.com/jtetteroo/MDBrowser',
+      author='Inzelton Co.',
+      author_email='something@example.com',
+      license='MIT',
+      packages=['mdbrowser'],
+      zip_safe=False)
